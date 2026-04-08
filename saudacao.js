80ef0d8b995eb87448cb00f52ef8bc203605ee9f
Júlia de O. M. <julia.martins25@escola.pr.gov.br>
2026-04-08 14:20:46 +0000

    versao2

--- saudacao.js
+++ saudacao.js
@@ -21,3 +21,14 @@ function soma(numero1, numero2){
 };
 
 console.log(soma(5, 3));
+
+// Verificar par
+function verificarPar (numero){
+    if (numero % 2 === 0){
+        return "O número é par."
+    } else {
+        return "O número é impar."
+    }
+};
+
+console.log(verificarPar(9));
