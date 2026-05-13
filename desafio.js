6ede708b88f121a7e9ac0fd3eba2251a787df54f
Júlia de O. M. <julia.martins25@escola.pr.gov.br>
2026-05-13 13:52:31 +0000

    versao1

---
+++ desafio.js
@@ -0,0 +1,14 @@
+// 1. VERDE: declarar 5 variáveis para rpresentar um produto.
+// 2.Crie duas variáveis numéricas e exiba uma subtração.
+// Fazer uma saída de froma interpolada.
+
+const nomeDoProduto = "bola";
+const categoriaDoProduto = "brinquedos";
+let precoDoProduto = 20;
+let desconto = 5;
+const nomeDaLoja = "Maringá";
+const resultado = precoDoProduto - desconto;
+
+console.log(`O produto ${nomeDoProduto}, da categoria ${categoriaDoProduto}, custa ${precoDoProduto} reais, na loja ${nomeDaLoja}.`);
+console.log(`O comprador recebeu um desconto de ${desconto} reais.`);
+console.log(`O preço final da compra foi ${resultado} reais.`);
