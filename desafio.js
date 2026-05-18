6de2d75268ae04235d1184ed8ae5d1afdb71e85b
Júlia de O. M. <julia.martins25@escola.pr.gov.br>
2026-05-18 13:51:41 +0000

    versao2

--- desafio.js
+++ desafio.js
@@ -1,5 +1,5 @@
 // 1. VERDE: declarar 5 variáveis para rpresentar um produto.
-// 2.Crie duas variáveis numéricas e exiba uma subtração.
+// 2. Crie duas variáveis numéricas e exiba uma subtração.
 // Fazer uma saída de froma interpolada.
 
 const nomeDoProduto = "bola";
@@ -12,3 +12,18 @@ const resultado = precoDoProduto - desconto;
 console.log(`O produto ${nomeDoProduto}, da categoria ${categoriaDoProduto}, custa ${precoDoProduto} reais, na loja ${nomeDaLoja}.`);
 console.log(`O comprador recebeu um desconto de ${desconto} reais.`);
 console.log(`O preço final da compra foi ${resultado} reais.`);
+
+// 3. Declarar preço e quantidade. Calcular o valor total, o valor com 20% de desconto, quantos produtos inteiros se compra com R$ 500,00 e quanto sobra de troco.
+
+const produto = "tenis";
+const preco = 100;
+let quantidade = 3;
+
+const total = produto * quantidade;
+const desconto = 20 / 100;
+const valoDoDesconto = preco * desconto;
+const totalComDesconto = preco - valoDoDesconto;
+
+console.log(`O produto ${produto} custa R$${preco}.`);
+console.log(`Um cliente comprou ${quantidade} unidades do produto`);
+console.log(`A loja estava com uma promoção: compre três produtos e guanhe um desconto de ${desconto * 100}%`);
